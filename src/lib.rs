4291192d89b4b973a58dbfcb1e444ae4e7dbbc73
TrialDragon <cj.2003@live.com>
2024-08-14 11:52:42 -0700

    Add input actions for player

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
+use bevy::prelude::*;
+use leafwing_input_manager::prelude::*;
 
+#[derive(Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
+enum Action {
+    Move,
+    Rotate,
+    Shoot,
+}
