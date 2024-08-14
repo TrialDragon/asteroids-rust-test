8d044db8040697e4d7388016cca65418591f9147
TrialDragon <cj.2003@live.com>
2024-08-14 15:11:59 -0700

    Add game states

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 use bevy::prelude::*;
 use leafwing_input_manager::prelude::*;
 
+pub mod player;
+
 #[derive(Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
 enum Action {
     Move,
     Rotate,
     Shoot,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Default, States)]
+pub enum GameState {
+    #[default]
+    Loading,
+    MainMenu,
+    Playing,
+    GameOver,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use bevy::prelude::*;
+use bevy_asset_loader::prelude::*;
+
+use game_library::player;
+use game_library::GameState;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .init_state::<GameState>()
         .run();
 }
