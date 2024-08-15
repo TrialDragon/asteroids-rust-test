60c57622cc0f322750dc4d683d40fc97253df6ad
TrialDragon <cj.2003@live.com>
2024-08-14 22:28:19 -0700

    Add basic player ship movement

--- src/player.rs
+++ src/player.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::*;
-use leafwing_input_manager::prelude::*;
-use bevy_asset_loader::prelude::*;
 use avian2d::prelude::*;
+use bevy::prelude::*;
+use bevy_asset_loader::prelude::*;
+use bevy_transform_interpolation::{RotationInterpolation, TranslationInterpolation};
+use leafwing_input_manager::prelude::*;
 
-use crate::{Action, GameState};
+use crate::{stats::{LinearAcceleration, AngularAcceleration}, Action, GameState};
 
 pub fn plugin(app: &mut App) {
     app.configure_loading_state(
@@ -38,6 +39,10 @@ fn spawn_player(mut commands: Commands, assets: Res<PlayerAssets>) {
         Player,
         RigidBody::Kinematic,
         Collider::circle(1.),
+        TranslationInterpolation,
+        RotationInterpolation,
+        LinearAcceleration(150.),
+        AngularAcceleration(2.),
         SpriteBundle {
             texture: assets.sprite.clone(),
             ..default()
@@ -46,4 +51,44 @@ fn spawn_player(mut commands: Commands, assets: Res<PlayerAssets>) {
     ));
 }
 
-fn move_player() {}
+// Need to allow this, otherwise trying to
+// break up the type to make it less complex
+// breaks adding the system to the app.
+#[allow(clippy::type_complexity)]
+fn move_player(
+    mut query: Query<(
+        &mut LinearVelocity,
+        &mut AngularVelocity,
+        &ActionState<Action>,
+        &LinearAcceleration,
+        &AngularAcceleration,
+        &Transform,
+    ),
+        With<Player>>,
+    time: Res<Time<Fixed>>
+) {
+    const MAX_LINEAR_SPEED: f32 = 300.0;
+    const MAX_ANGULAR_SPEED: f32 = 3.;
+
+    for (mut linear_velocity, mut angular_velocity, action_state, linear_acceleration, angular_acceleration, transform) in &mut query {
+        let throttle = action_state.clamped_value(&Action::Move);
+        let rotation = action_state.clamped_value(&Action::Rotate);
+        let direction = transform.rotation * Vec3::Y;
+
+        if throttle == 0.0 {
+            linear_velocity.0 = linear_velocity.0.move_towards(Vec2::ZERO, linear_acceleration.0 * time.delta_seconds());
+        }
+
+        if rotation == 0.0 {
+            angular_velocity.0 = angular_velocity.0.lerp(0.0, angular_acceleration.0 * time.delta_seconds());
+        }
+
+        // Accelerate linear velocity 
+        linear_velocity.0 = linear_velocity.0.move_towards(direction.xy() * MAX_LINEAR_SPEED, linear_acceleration.0 * throttle * time.delta_seconds());
+
+
+        // It appears negative rotates right and positive left
+        // so this needs to be inverted to get correct rotations.
+        angular_velocity.0 = angular_velocity.0.lerp(MAX_ANGULAR_SPEED * -rotation, angular_acceleration.0 * time.delta_seconds());
+    }
+}
