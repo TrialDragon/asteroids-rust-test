b3d0d1cff40273f569f629a105ba1109dba4dbcd
TrialDragon <cj.2003@live.com>
2024-08-15 17:49:57 -0700

    Add a health stat
    Closes #12

--- src/stats.rs
+++ src/stats.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 pub use bevy::prelude::*;
 
 #[derive(Component, Reflect)]
@@ -7,3 +9,34 @@ pub struct LinearAcceleration(pub f32);
 #[derive(Component, Reflect)]
 #[reflect(Component)]
 pub struct AngularAcceleration(pub f32);
+
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+pub struct Health {
+    max: u16,
+    current:u16
+}
+
+impl Health {
+    pub fn new(max: u16) -> Self {
+        Self {
+            max,
+            current: 0,
+        }
+    }
+
+    pub fn add(&mut self, health: u16) {
+        self.current = self.current.saturating_add(health);
+        self.current = cmp::min(self.current, self.max);
+    }
+
+    pub fn sub(&mut self, health: u16) {
+        self.current = self.current.saturating_sub(health);
+    }
+
+    pub fn current(&self) -> u16 {
+        self.current
+    }
+}
+
+
