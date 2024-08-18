b1c25aac16b002c17f24151a137b2ad86acc2d69
TrialDragon <cj.2003@live.com>
2024-08-17 20:41:21 -0700

    Format code

--- src/game_over.rs
+++ src/game_over.rs
@@ -1,11 +1,17 @@
-use bevy::{color::palettes::{css::BLACK, tailwind::GRAY_50}, prelude::*};
+use bevy::{
+    color::palettes::{css::BLACK, tailwind::GRAY_50},
+    prelude::*,
+};
 use sickle_ui::prelude::*;
 
 use crate::GameState;
 
 pub fn plugin(app: &mut App) {
     app.add_systems(OnEnter(GameState::GameOver), setup_game_over);
-    app.add_systems(Update, (restart_button, title_button).run_if(in_state(GameState::GameOver)));
+    app.add_systems(
+        Update,
+        (restart_button, title_button).run_if(in_state(GameState::GameOver)),
+    );
 }
 
 #[derive(Component, Reflect)]
@@ -40,46 +46,54 @@ fn title_button(
 
 fn setup_game_over(mut commands: Commands) {
     // UI root.
-    commands.ui_builder(UiRoot).column(|column| {
-        // Title text.
-        column.spawn(TextBundle::from("Game Over")).style()
-            .margin(UiRect::top(Val::Vh(15.)));
+    commands
+        .ui_builder(UiRoot)
+        .column(|column| {
+            // Title text.
+            column
+                .spawn(TextBundle::from("Game Over"))
+                .style()
+                .margin(UiRect::top(Val::Vh(15.)));
 
-        // Button menu.
-        column.column(|column| {
-            // Restart button.
-            column.container((
-                ButtonBundle::default(),
-                RestartButton,
-            ),
-            |button| {
-                button.spawn(TextBundle::from("Restart")).style()
-                    .align_self(AlignSelf::Center)
-                    .font_color(BLACK.into());
-            }).style()
-                .background_color(GRAY_50.into())
-                .padding(UiRect::horizontal(Val::Px(20.)));
+            // Button menu.
+            column
+                .column(|column| {
+                    // Restart button.
+                    column
+                        .container((ButtonBundle::default(), RestartButton), |button| {
+                            button
+                                .spawn(TextBundle::from("Restart"))
+                                .style()
+                                .align_self(AlignSelf::Center)
+                                .font_color(BLACK.into());
+                        })
+                        .style()
+                        .background_color(GRAY_50.into())
+                        .padding(UiRect::horizontal(Val::Px(20.)));
 
-            // Title button.
-            column.container((
-                ButtonBundle::default(),
-                TitleButton,
-            ),
-            |button| {
-                button.spawn(TextBundle::from("Title")).style()
-                    .align_self(AlignSelf::Center)
-                    .font_color(BLACK.into());
-            }).style()
-                .background_color(GRAY_50.into())
-                .padding(UiRect::horizontal(Val::Px(20.)));
-        }).style()
-            .height(Val::Percent(10.))
-            .justify_content(JustifyContent::SpaceAround)
-            .margin(UiRect::bottom(Val::Vh(45.)));
-    }).style()
+                    // Title button.
+                    column
+                        .container((ButtonBundle::default(), TitleButton), |button| {
+                            button
+                                .spawn(TextBundle::from("Title"))
+                                .style()
+                                .align_self(AlignSelf::Center)
+                                .font_color(BLACK.into());
+                        })
+                        .style()
+                        .background_color(GRAY_50.into())
+                        .padding(UiRect::horizontal(Val::Px(20.)));
+                })
+                .style()
+                .height(Val::Percent(10.))
+                .justify_content(JustifyContent::SpaceAround)
+                .margin(UiRect::bottom(Val::Vh(45.)));
+        })
+        .style()
         .width(Val::Percent(50.))
         .justify_content(JustifyContent::SpaceBetween)
         .align_items(AlignItems::Center)
         .margin(UiRect::all(Val::Auto))
-        .entity_commands().insert(StateScoped(GameState::GameOver));
+        .entity_commands()
+        .insert(StateScoped(GameState::GameOver));
 }
--- src/title.rs
+++ src/title.rs
@@ -1,4 +1,7 @@
-use bevy::{color::palettes::{css::BLACK, tailwind::GRAY_50}, prelude::*};
+use bevy::{
+    color::palettes::{css::BLACK, tailwind::GRAY_50},
+    prelude::*,
+};
 use sickle_ui::prelude::*;
 
 use crate::GameState;
@@ -7,7 +10,10 @@ use crate::GameState;
 // but no time for that at this point.
 pub fn plugin(app: &mut App) {
     app.add_systems(OnEnter(GameState::Title), setup_title);
-    app.add_systems(Update, (exit_button, play_button).run_if(in_state(GameState::Title)));
+    app.add_systems(
+        Update,
+        (exit_button, play_button).run_if(in_state(GameState::Title)),
+    );
 }
 
 #[derive(Component, Reflect)]
@@ -15,10 +21,7 @@ pub fn plugin(app: &mut App) {
 struct PlayButton;
 
 fn play_button(
-    query: Query<
-        &Interaction,
-        (Changed<Interaction>, With<PlayButton>)
-    >,
+    query: Query<&Interaction, (Changed<Interaction>, With<PlayButton>)>,
     mut next_state: ResMut<NextState<GameState>>,
 ) {
     for interaction in &query {
@@ -33,10 +36,7 @@ fn play_button(
 struct ExitButton;
 
 fn exit_button(
-    query: Query<
-        &Interaction,
-        (Changed<Interaction>, With<ExitButton>)
-    >,
+    query: Query<&Interaction, (Changed<Interaction>, With<ExitButton>)>,
     mut exit_event_writer: EventWriter<AppExit>,
 ) {
     for interaction in &query {
@@ -47,37 +47,47 @@ fn exit_button(
 }
 
 fn setup_title(mut commands: Commands) {
-    commands.ui_builder(UiRoot).column(|column| {
-        column.spawn(TextBundle::from("Asteroids")).style()
-            .margin(UiRect::top(Val::Vh(15.)));
+    commands
+        .ui_builder(UiRoot)
+        .column(|column| {
+            column
+                .spawn(TextBundle::from("Asteroids"))
+                .style()
+                .margin(UiRect::top(Val::Vh(15.)));
 
-        column.column(|column| {
-            column.container((
-                ButtonBundle::default(),
-                PlayButton,
-            ), |button| {
-                button.spawn(TextBundle::from("Play")).style()
-                    .font_color(BLACK.into());
-            }).style()
-                .background_color(GRAY_50.into())
-                .padding(UiRect::horizontal(Val::Px(20.)));
-            column.container((
-                ButtonBundle::default(),
-                ExitButton,
-            ), |button| {
-                button.spawn(TextBundle::from("Exit")).style()
-                    .font_color(BLACK.into());
-            }).style()
-                .background_color(GRAY_50.into())
-                .padding(UiRect::horizontal(Val::Px(20.)));
-        }).style()
-            .height(Val::Percent(10.))
-            .justify_content(JustifyContent::SpaceAround)
-            .margin(UiRect::bottom(Val::Vh(45.)));
-    }).style()
+            column
+                .column(|column| {
+                    column
+                        .container((ButtonBundle::default(), PlayButton), |button| {
+                            button
+                                .spawn(TextBundle::from("Play"))
+                                .style()
+                                .font_color(BLACK.into());
+                        })
+                        .style()
+                        .background_color(GRAY_50.into())
+                        .padding(UiRect::horizontal(Val::Px(20.)));
+                    column
+                        .container((ButtonBundle::default(), ExitButton), |button| {
+                            button
+                                .spawn(TextBundle::from("Exit"))
+                                .style()
+                                .font_color(BLACK.into());
+                        })
+                        .style()
+                        .background_color(GRAY_50.into())
+                        .padding(UiRect::horizontal(Val::Px(20.)));
+                })
+                .style()
+                .height(Val::Percent(10.))
+                .justify_content(JustifyContent::SpaceAround)
+                .margin(UiRect::bottom(Val::Vh(45.)));
+        })
+        .style()
         .width(Val::Px(50.0))
         .justify_content(JustifyContent::SpaceBetween)
         .align_items(AlignItems::Center)
         .margin(UiRect::all(Val::Auto))
-        .entity_commands().insert(StateScoped(GameState::Title));
+        .entity_commands()
+        .insert(StateScoped(GameState::Title));
 }
